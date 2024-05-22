d88ec0a928c3d4be171d60c54407711b7ef86b52
diana56178 <yusupova.diana.94@mail.ru>
2024-05-22 14:40:12 +0500

    рефакторинг кода

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import EventPresenter from './presenter/event-presenter.js';
 import PointsModel from './model/points-model.js';
 import DestinationModel from './model/destination-model.js';
-import OffersModel from "./model/offers-model.js";
+import OffersModel from './model/offers-model.js';
 
 const siteMainElement = document.querySelector('.page-body');
 
