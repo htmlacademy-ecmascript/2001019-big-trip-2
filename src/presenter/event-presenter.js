f1434712cafaebd79796ca928202e4f70cf324fe
diana56178 <yusupova.diana.94@mail.ru>
2024-08-21 20:01:18 +0500

    добавила условие непоказа сообщения о создании новой точки при наличии точек

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -187,6 +187,8 @@ export default class EventPresenter {
   }
 
   renderNoPointComponent() {
+    if (this.points.length > 0) return;
+
     this.#noPointComponent = new NoPointView({
       filterType: this.#filterType
     });
